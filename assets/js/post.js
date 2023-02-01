35c40dae14f2f5f8c63431b68fd76747c5aef93a
programmingsockenthusiast <s.knauer@protonmail.com>
2023-02-01 12:28:19 +0100

    unfuck stuff pls

--- assets/js/post.js
+++ assets/js/post.js
@@ -9,7 +9,7 @@ function getBlogPost() {
 
 function loadPost(postId) {
   $(document).ready(function () {
-    $.get("/blog/posts/" + postId + ".html", function (data) {
+    $.get("/blog/posts/" + postId + ".html", function (data) {  /* <---- HIER DIE URL ANPASSEN */
       var postTitle = $(data).find(".post_title").text();
       var postDate = $(data).find(".post_date").text();
       var postContent = $(data).find(".post_content").text();
@@ -29,52 +29,43 @@ function loadPost(postId) {
   });
 }
 
-function loadPostList() {
-  $(document).ready(function () {
-    var postListHtml = "";
-    $.ajax({
-      url: "/blog/posts/",
-      success: function (data) {
-        $(data)
-          .find("a")
-          .each(function () {
-            var postLink = $(this).attr("href");
-
-            if (postLink.endsWith(".html")) {
-              var postId = postLink
-                .substr(0, postLink.length - 5)
-                .split("/")
-                .pop();
-              $.get("/blog/posts/" + postId + ".html", function (postData) {
-                var postTitle = $(postData).find(".post_title").text();
-                var postDate = $(postData).find(".post_date").text();
-                var postContent = $(postData)
-                  .find(".post_content")
-                  .text()
-                  .split(" ")
-                  .slice(0, 51)
-                  .join(" ");
-
-                postListHtml +=
-                  '<div class="small_header"><a href="/blog/" class="blog-link" data-post-id="' +
-                  postId +
-                  '"><div class="prev_title">' +
-                  postTitle +
-                  '</div><div class="prev_date">(' +
-                  postDate +
-                  ')</div></a></div><div class="small_body"><p>' +
-                  postContent +
-                  '... <a href="/blog/?postId=' +
-                  postId +
-                  '">  read more</a></div>';
-                $("#blog-content").html(postListHtml);
-              });
-            }
-          });
-          BlogClick()
-      },
-    });
+async function loadPostList() {
+  const postListHtml = [];
+  const data = await $.ajax({
+    url: "/Website/blog/posts/posts.json",   /* <---- HIER DIE URL ANPASSEN */
   });
+
+  for (let i = 0; i < data.posts.length; i++) {
+    const postId = data.posts[i].id;
+    const postUrl = "/Website/blog/posts/" + postId + ".html";  /* <---- HIER DIE URL ANPASSEN */
+    const postData = await $.get(postUrl);
+
+    const postTitle = $(postData).find(".post_title").text();
+    const postDate = $(postData).find(".post_date").text();
+    const postContent = $(postData)
+      .find(".post_content")
+      .text()
+      .split(" ")
+      .slice(0, 51)
+      .join(" ");
+
+    postListHtml.push(
+      `<section>
+        <div class="small_header">
+          <a href="/blog/" class="blog-link" data-post-id="${postId}">
+            <div class="prev_title">${postTitle}</div>
+            <div class="prev_date">(${postDate})</div>
+          </a>
+        </div>
+        <div class="small_body">
+          <p>${postContent}... <a href="/blog/?postId=${postId}">  read more</a></p>  
+        </div>
+      </section>`
+    );                                                      /* <---- HIER DIE URL ANPASSEN (zeile 61) */
+  }  
+
+  $("#blog-content").html(postListHtml.join(""));
+  BlogClick();
 }
 
 function getUrlParameter(name) {
@@ -91,7 +82,7 @@ function BlogClick() {
     $(document).on("click", ".blog-link", function (event) {
       event.preventDefault();
       var postId = $(this).data("post-id");
-      window.location.href = "/blog/?postId=" + postId;
+      window.location.href = "/blog/?postId=" + postId;  /* <---- HIER DIE URL ANPASSEN */
     });
   });
 }
